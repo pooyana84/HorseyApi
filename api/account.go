309973257c04c1df9142da1db46910aff85f9189
pooya <pooyana84@gmail.com>
2023-02-25 14:49:00 +0330

    fix GetProfile struct

--- api/account.go
+++ api/account.go
@@ -4,14 +4,14 @@ import "net/http"
 import "encoding/json"
 import "io/ioutil"
 
-func (profile *Profile) GetProfile (token string, client *http.Client) (string) {
+func (account *Account) GetProfile (token string, client *http.Client) (string) {
 	request, err := NewRequest(token, "GET", LichessUrl + "/api/account", nil)
 	checkErr(err)
 	response, err := client.Do(request)
 	checkErr(err)
 	defer response.Body.Close()
 	responseBody , err := ioutil.ReadAll(response.Body)
-	err = json.Unmarshal(responseBody, profile)
+	err = json.Unmarshal(responseBody, account)
 	checkErr(err)
 	return response.Status
 } 
